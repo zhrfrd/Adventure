39d544392167f748a1a4c2cb9716b39df097faa8
Farid Zouheir <zouheirjob@hotmail.com>
2022-09-04 10:47:07 +0100

    feat: add player sprite and basic movement animation

---
+++ src/zhrfrd/adventure/entities/Player.java
@@ -0,0 +1,134 @@
+package zhrfrd.adventure.entities;
+
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+import zhrfrd.adventure.main.GamePanel;
+import zhrfrd.adventure.main.KeyHandler;
+
+public class Player extends Entity {
+	GamePanel gamePanel;
+	KeyHandler keyHandler;
+	
+	public Player(GamePanel gamePanel, KeyHandler keyHandler) {
+		this.gamePanel = gamePanel;
+		this.keyHandler = keyHandler;
+		
+		setDefaultValues();
+		getPlayerImage();
+	}
+	
+	/*
+	 * Set default values of the player when a new instance of Player is created
+	 */
+	public void setDefaultValues() {
+		x = 100;
+		y = 100;
+		speed = 4;
+		direction = "down";   // Default direction
+	}
+	
+	/*
+	 * Get the sprites of the player
+	 */
+	public void getPlayerImage() {
+		try {
+			up1 = ImageIO.read(getClass().getResource("/player/player_up_1.png"));
+			up2 = ImageIO.read(getClass().getResource("/player/player_up_2.png"));
+			down1 = ImageIO.read(getClass().getResource("/player/player_down_1.png"));
+			down2 = ImageIO.read(getClass().getResource("/player/player_down_2.png"));
+			left1 = ImageIO.read(getClass().getResource("/player/player_left_1.png"));
+			left2 = ImageIO.read(getClass().getResource("/player/player_left_2.png"));
+			right1 = ImageIO.read(getClass().getResource("/player/player_right_1.png"));
+			right2 = ImageIO.read(getClass().getResource("/player/player_right_2.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/*
+	 * Update player information
+	 */
+	public void update() {
+		// Get keyboard strokes and update player position
+		if (keyHandler.upPressed || keyHandler.downPressed || keyHandler.leftPressed || keyHandler.rightPressed) {
+			if (keyHandler.upPressed) {
+				direction = "up";
+				y -= speed;
+			}
+			
+			if (keyHandler.downPressed) {
+				direction = "down";
+				y += speed;
+			}
+			
+			if (keyHandler.leftPressed) {
+				direction = "left";
+				x -= speed;
+			}
+			
+			if (keyHandler.rightPressed) {
+				direction = "right";
+				x += speed;
+			}
+			
+			spriteCounter ++;   // Increase spriteCounter every 0.01666 seconds
+			
+			// Swap sprite every 10 FPS
+			if (spriteCounter > 9) {
+				if (spriteNumber == 1)
+					spriteNumber = 2;
+				
+				else
+					spriteNumber = 1;
+				
+				spriteCounter = 0;
+			}
+		}
+	}
+	
+	/*
+	 * Re-sraw player each update
+	 */
+	public void draw(Graphics2D g2) {
+		BufferedImage image = null;
+		
+		if (direction == "up") {
+			if (spriteNumber == 1)
+				image = up1;
+			
+			else
+				image = up2;
+		}
+		
+		if (direction == "down") {
+			if (spriteNumber == 1)
+				image = down1;
+			
+			else
+				image = down2;
+		}
+		
+		if (direction == "left") {
+			if (spriteNumber == 1)
+				image = left1;
+			
+			else
+				image = left2;
+		}
+		
+		if (direction == "right") {
+			if (spriteNumber == 1)
+				image = right1;
+			
+			else
+				image = right2;
+		}
+		
+		g2.drawImage(image, x, y, gamePanel.TILE_SIZE, gamePanel.TILE_SIZE, null);
+	}
+}
--- src/zhrfrd/adventure/main/GamePanel.java
+++ src/zhrfrd/adventure/main/GamePanel.java
@@ -7,20 +7,27 @@ import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import zhrfrd.adventure.entities.Player;
+
 public class GamePanel extends JPanel implements Runnable {
-	final int originalTileSize = 16;   // 16x16 pixels tile
-	final int scale = 3;   // Rescale tiles 
-	final int tileSize = originalTileSize * scale;   // 48*48 pixels tile
-	final int maxScreenCol = 16;   // Max tiles to display on the screen
-	final int maxScreenRow = 12;   // 
-	final int screenWidth = tileSize * maxScreenCol;    // 768 pixels
-	final int screenHeight = tileSize * maxScreenRow;   // 576 pixels
+	final int ORIGINAL_TILE_SIZE = 16;   // 16x16 pixels tile
+	final int SCALE = 3;   // Re-scale tiles 
+	public final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE;   // 48*48 pixels tile
+	final int MAX_SCREEN_COL = 16;   // Max tiles to display on the screen
+	final int MAX_SCREEN_ROW = 12;   // 
+	final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL;    // 768 pixels
+	final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW;   // 576 pixels
+	final int FPS = 60;
 	Thread gameThread;
+	KeyHandler keyHandler = new KeyHandler();
+	Player player = new Player(this, keyHandler);
 	
 	public GamePanel() {
-		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+		this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyHandler);
+		this.setFocusable(true);
 	}
 	
 	/*
@@ -35,7 +42,7 @@ public class GamePanel extends JPanel implements Runnable {
 	 * Update information such as character position
 	 */
 	public void update() {
-		
+		player.update();
 	}
 	
 	/*
@@ -44,18 +51,32 @@ public class GamePanel extends JPanel implements Runnable {
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);   // Must be done for the JPanel painting takes place
-		Graphics2D g2 = (Graphics2D)g;
 		
-		g2.setColor(Color.white);
-		g2.fillRect(100, 100, tileSize, tileSize);
+		Graphics2D g2 = (Graphics2D)g;	
+		player.draw(g2);
 		g2.dispose();   // Dispose g2 in order to save resources
 	}
 
 	@Override
 	public void run () {
+		double drawInterval = 1000000000 / FPS;   // Draw every 0.01666 seconds
+		double delta = 0;
+		long lastTime = System.nanoTime();
+		long currentTime;
+		
+		// Game loop
 		while(gameThread != null) {
-			update();   // Update information such as character position
-			repaint();   // Call paintComponent() to draw the screen with the updated information
+			currentTime = System.nanoTime();
+			delta += (currentTime - lastTime) / drawInterval;
+			lastTime = currentTime;
+			
+			// Every 0.01666 seconds (60 FPS)
+			if (delta >= 1) {
+				update();   // Update information such as character position
+				repaint();   // Call paintComponent() to draw the screen with the updated information
+				
+				delta --;
+			}
 		}
 	}
 }
